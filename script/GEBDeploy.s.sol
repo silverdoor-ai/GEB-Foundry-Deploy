077ebe12a5fa8ab11f54e73f266fa056d0e787c5
st4rgard3n <stargardenbeats@gmail.com>
2023-09-24 18:02:30 -0700

    config

--- script/GEBDeploy.s.sol
+++ script/GEBDeploy.s.sol
@@ -2,11 +2,65 @@
 pragma solidity ^0.6.7;
 
 import "forge-std/Script.sol";
+import {DSAuth, DSAuthority} from "ds-auth/auth.sol";
+import {DSPause, DSPauseProxy} from "ds-pause/pause.sol";
+import {DSProtestPause} from "ds-pause/protest-pause.sol";
+
+import {SAFEEngine} from "geb/SAFEEngine.sol";
+import {TaxCollector} from "geb/TaxCollector.sol";
+import {AccountingEngine} from "geb/AccountingEngine.sol";
+import {LiquidationEngine} from "geb/LiquidationEngine.sol";
+import {CoinJoin} from "geb/BasicTokenAdapters.sol";
+import {RecyclingSurplusAuctionHouse, BurningSurplusAuctionHouse} from "geb/SurplusAuctionHouse.sol";
+import {DebtAuctionHouse} from "geb/DebtAuctionHouse.sol";
+import {EnglishCollateralAuctionHouse, IncreasingDiscountCollateralAuctionHouse} from "geb/CollateralAuctionHouse.sol";
+import {Coin} from "geb/Coin.sol";
+import {GlobalSettlement} from "geb/GlobalSettlement.sol";
+import {ESM} from "esm/ESM.sol";
+import {StabilityFeeTreasury} from "geb/StabilityFeeTreasury.sol";
+import {CoinSavingsAccount} from "geb/CoinSavingsAccount.sol";
+import {OracleRelayer} from "geb/OracleRelayer.sol";
+
+abstract contract CollateralAuctionHouse {
+    function modifyParameters(bytes32, uint256) virtual external;
+    function modifyParameters(bytes32, address) virtual external;
+}
+
+abstract contract AuthorizableContract {
+    function addAuthorization(address) virtual external;
+    function removeAuthorization(address) virtual external;
+}
 
 contract GEBDeploy is Script {
+
+    SAFEEngine                        public safeEngine;
+    TaxCollector                      public taxCollector;
+    AccountingEngine                  public accountingEngine;
+    LiquidationEngine                 public liquidationEngine;
+    StabilityFeeTreasury              public stabilityFeeTreasury;
+    Coin                              public coin;
+    CoinJoin                          public coinJoin;
+    RecyclingSurplusAuctionHouse      public recyclingSurplusAuctionHouse;
+    BurningSurplusAuctionHouse        public burningSurplusAuctionHouse;
+    DebtAuctionHouse                  public debtAuctionHouse;
+    OracleRelayer                     public oracleRelayer;
+    CoinSavingsAccount                public coinSavingsAccount;
+    GlobalSettlement                  public globalSettlement;
+    ESM                               public esm;
+    DSPause                           public pause;
+    DSProtestPause                    public protestPause;
+
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 privKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.rememberKey(privKey);
+        vm.startBroadcast(deployer);
+
+        safeEngine = new SAFEEngine();
+
+        liquidationEngine = new LiquidationEngine(address(safeEngine));
+
+        taxCollector = new TaxCollector(address(safeEngine));
     }
 }
